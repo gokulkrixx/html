ced0b14d8512df80d997b6efbb76f2b8ec53ff60
gokulkrixx <gk503255@gmail.com>
2024-02-14 11:35:26 +0530

    hai

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hello react</h1>
+    <h2>hello react</h2>
+    </>
+  )
+}
+
+export default App
