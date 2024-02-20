ad823a7254faba0d1a3a38d5f64751ad1ebde309
gokulkrixx <gk503255@gmail.com>
2024-02-20 11:35:50 +0530

    heee

---
+++ vite-project/src/routing/BasicRouting/BasicRouting.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {BrowserRouter as Routes, Route, Link} from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import About from './About';
+
+function BasicRouting(){
+    return(
+        <Router>
+
+        </Router>
+    )
+}
