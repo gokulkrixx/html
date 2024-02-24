d3df0b43eb6ada6a7e9d1ab10f3a902b089d086e
gokulkrixx <gk503255@gmail.com>
2024-02-24 10:56:09 +0530

    hello route

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require ('express');
 const app = express();
+const dotenv = require('dotenv');
 const port = process.env.PORT;
 dotenv.config();
 
 
 app.get('/test',(req,res,)=>{
-    res.status(200).send("sucess");
+    res.status("hello route");
 })
+
+
+app.listen(port,()=>{
+    console.log(`Server listeing at http://localhost${process.env.PORT}`)
+});
